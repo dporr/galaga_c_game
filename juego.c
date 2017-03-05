98cb642b4d2cd94d1c45bc26b881a3d45f6d3a05
Diego Porras <andree.rivas@galileo.edu>
2017-03-04 22:34:41 -0600

    Refactorizacion completa del juego- toFix:Display

--- juego.c
+++ juego.c
@@ -1,22 +1,30 @@
+//Standar library includes
 #include <stdio.h>
 #include <stdlib.h>
+//Allegro framework includes
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/keyboard.h>
+//Own includes
+#include "enemies.h"
+#include "player.h"
 
-#define FPS 30.0
+#define FPS 60.0
 #define CANTMOV 5
-#define BACK "wall.png"
+#define BACK "res/wall.png"
+#define SCREEN_W 640
+#define SCREEN_H 1000
 
+//prototipado de funciones
+int init_framework_components();
+
+//creacion de variables
+ALLEGRO_DISPLAY *display = NULL;
+ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+ALLEGRO_TIMER *timer = NULL;
+ALLEGRO_BITMAP *buffer;
 enum KEYS{UP, DOWN, LEFT, RIGHT };
 int teclas[4] = {0, 0, 0, 0};
-
-
-typedef struct jugador {
-  int x; // posicion x de la nave
-  int y; // posicion y de la nave
-  ALLEGRO_BITMAP *nave; //imagen a renderizar
-} jugador_t;
 
 typedef struct fondo {
   int x; // posicion x de la nave
@@ -24,85 +32,73 @@ typedef struct fondo {
   ALLEGRO_BITMAP *fnave; //imagen a renderizar
 } fondo_t;
 
-// funciones
-void dibujarJugador(jugador_t *jugador);
-void dibujarFondo(fondo_t *fondo);
-void moverArriba(jugador_t *jugador);
-void moverAbajo(jugador_t *jugador);
-void moverDerecha(jugador_t *jugador);
-void moverIzquierda(jugador_t *jugador);
+void dibujarFondo(fondo_t *fondo) {
+  al_draw_bitmap(fondo->fnave, fondo->x, fondo->y, 0);
+  al_flip_display();
+}
+int main(int argc, char **argv){
+  init_framework_components();
 
-int main(int argc, char **argv) {
-
-  //creacion de variables
-  ALLEGRO_DISPLAY *display = NULL;
-  ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-  ALLEGRO_TIMER *timer = NULL;
-  //inicializacion del librerias
-  if(!al_init()) { fprintf(stderr, "%s\n", "No se pudo inicializar allegro 5"); return -1;  }
-  if(!al_init_image_addon()) { fprintf(stderr,"%s\n", "No se pudo inicializar el addon"); al_destroy_display(display); return -1; }
-  display = al_create_display(1000, 600);
-  if(!display) { fprintf(stderr, "%s\n", "No se pudo crear un display"); al_destroy_display(display);   return -1;  } 
-  if(!al_install_keyboard()) {    fprintf(stderr, "%s\n", "No se pudo instalar el teclado");   al_destroy_display(display);    return -1; }
-  
+  //Configuraciones miscelaneas
   al_inhibit_screensaver(1);//evitar suspencion de pc
   al_set_window_title(display, "Space"); //titulo de la ventana
-  timer = al_create_timer(1.0 / FPS); //creamos el timer
 
-  // Creamos los eventos del juego
-  event_queue = al_create_event_queue();
+  //Registramos las fuentes de eventos
   al_register_event_source(event_queue, al_get_keyboard_event_source());
   al_register_event_source(event_queue, al_get_timer_event_source(timer));
 
-  jugador_t *player = (jugador_t *)malloc(sizeof(jugador_t));
-  player->nave = al_load_bitmap("res/spacecraft.png");
-  player->x = 0;
-  player->y = 480;
+  //Inicializamos al jugador principal
+  player = init_player();
 
   fondo_t *fondo = (fondo_t *)malloc(sizeof(fondo_t));
-  fondo->fnave = al_load_bitmap("res/wall.jpg");
+  fondo->fnave = al_load_bitmap(BACK);
   fondo->x = 0;
   fondo->y = 0;
   
-  if(!player->nave) {    fprintf(stderr, "%s\n", "No se pudo crear un display");
-    al_destroy_display(display); al_destroy_event_queue(event_queue); al_destroy_timer(timer);
+  if(!player->nave) {
+    fprintf(stderr, "%s\n", "No se pudo crear un display");
+    al_destroy_display(display); 
+    al_destroy_event_queue(event_queue);
+    al_destroy_timer(timer);
     return -1;
   }
   srand(time(NULL));
   al_start_timer(timer);//comenzamos el timer
   int terminar = 0; //teclado para salir
   ALLEGRO_EVENT ev;//variable que recive evento
-
   dibujarFondo(fondo);
-  
+  level_generator(enemies_r);
   while(!terminar) {//cuerpo del juego
     al_wait_for_event(event_queue, &ev);
     if(ev.type == ALLEGRO_EVENT_KEY_UP) {
       switch(ev.keyboard.keycode) {
-      case ALLEGRO_KEY_ESCAPE:	terminar = 1;   break;
-      case ALLEGRO_KEY_UP:      teclas[UP]=0;	break;
-      case ALLEGRO_KEY_DOWN:	teclas[DOWN]=0;	break;
-      case ALLEGRO_KEY_LEFT:	teclas[LEFT]=0;	break;
-      case ALLEGRO_KEY_RIGHT:	teclas[RIGHT]=0;break;
+        case ALLEGRO_KEY_ESCAPE:	terminar = 1; break;
+        case ALLEGRO_KEY_UP:      teclas[UP]=0;	break;
+        case ALLEGRO_KEY_DOWN:	teclas[DOWN]=0;	break;
+        case ALLEGRO_KEY_LEFT:	teclas[LEFT]=0;	break;
+        case ALLEGRO_KEY_RIGHT:	teclas[RIGHT]=0;break;
       }
-    } else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
-      switch(ev.keyboard.keycode) {
-      case ALLEGRO_KEY_UP:	teclas[UP]=1;	break;
-      case ALLEGRO_KEY_DOWN:	teclas[DOWN]=1;	break;
-      case ALLEGRO_KEY_LEFT:	teclas[LEFT]=1;	break;
-      case ALLEGRO_KEY_RIGHT:	teclas[RIGHT]=1;break;
+    } else if (ev.type == ALLEGRO_EVENT_KEY_DOWN){
+      switch(ev.keyboard.keycode){
+        case ALLEGRO_KEY_UP:	teclas[UP]=1;	break;
+        case ALLEGRO_KEY_DOWN:	teclas[DOWN]=1;	break;
+        case ALLEGRO_KEY_LEFT:	teclas[LEFT]=1;	break;
+        case ALLEGRO_KEY_RIGHT:	teclas[RIGHT]=1;break;
       }
     } else if(ev.type == ALLEGRO_EVENT_TIMER) {
-            if(teclas[UP]){	moverArriba(player);   dibujarFondo(fondo);
-      }else if(teclas[DOWN]){	moverAbajo(player);    dibujarFondo(fondo);
-      }else if(teclas[LEFT]){	moverIzquierda(player);dibujarFondo(fondo);
-      }else if(teclas[RIGHT]){	moverDerecha(player);  dibujarFondo(fondo);
+      if(teclas[UP]){
+       	moverArriba(player);
+      }else if(teclas[DOWN]){
+      	moverAbajo(player);
+      }else if(teclas[LEFT]){	
+        moverIzquierda(player);
+      }else if(teclas[RIGHT]){
+        moverDerecha(player);
       }
-    }
-    
-    dibujarJugador(player);
+    } 
+    // dibujamos al jugador
   }
-  // siemple hay que limpiar memoria
+  // siempre hay que limpiar memoria
   al_destroy_display(display);
   al_destroy_event_queue(event_queue);
   al_destroy_bitmap(fondo->fnave);
@@ -112,10 +108,29 @@ int main(int argc, char **argv) {
   return 0;
 }
 
-//desarrollo de funciones
-void dibujarJugador(jugador_t *jugador){ al_draw_bitmap(jugador->nave, jugador->x, jugador->y, 0); al_flip_display();}
-void dibujarFondo(fondo_t *fondo)      { al_draw_bitmap(fondo->fnave, fondo->x, fondo->y, 0);      al_flip_display();}
-void moverArriba(jugador_t *jugador)   { if(jugador->y == 0){   jugador->y = 0;   }else{    jugador->y -= 10;  }}
-void moverAbajo(jugador_t *jugador)    { if(jugador->y == 540){ jugador->y = 540; }else{    jugador->y += 10; }}
-void moverDerecha(jugador_t *jugador)  { if(jugador->x == 950){ jugador->x = 950; }else{    jugador->x += 10;  }}
-void moverIzquierda(jugador_t *jugador){ if(jugador->x == 0){   jugador->x = 0;   }else{    jugador->x -= 10;  }}
+int init_framework_components(){
+    //inicializacion del librerias
+  if(!al_init()) { 
+    fprintf(stderr, "%s\n", "No se pudo inicializar allegro 5"); 
+    return -1; 
+  }
+  if(!al_init_image_addon()){ 
+    fprintf(stderr,"%s\n", "No se pudo inicializar el addon");
+    al_destroy_display(display);
+    return -1; 
+  }
+  display = al_create_display(SCREEN_H, SCREEN_W);
+  if(!display){ 
+    fprintf(stderr, "%s\n", "No se pudo crear un display"); 
+    al_destroy_display(display);  
+    return -1;
+  } 
+  if(!al_install_keyboard()){
+    fprintf(stderr, "%s\n", "No se pudo instalar el teclado");
+    al_destroy_display(display);
+    return -1; 
+  }
+  timer = al_create_timer(1.0 / FPS); //Inicializo imer del juego
+  event_queue = al_create_event_queue(); //Cola global para eventos
+  return 0;
+}
