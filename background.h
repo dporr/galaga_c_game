786dc6cae6b93dfc3aa035b89c0d2504edd9a358
Diego Porras <andree.rivas@galileo.edu>
2017-03-05 00:28:22 -0600

    Manejamos los backgrounds en distinto source - facilidad de manejar niveles

---
+++ background.h
@@ -0,0 +1,16 @@
+#define BACK "res/wall.jpg"
+typedef struct fondo {
+  int x; // posicion x de la nave
+  int y; // posicion y de la nave
+  ALLEGRO_BITMAP *bg_image; //imagen a renderizar
+} fondo_t;
+
+fondo_t *fondo;
+fondo_t* init_background(){
+  fondo = (fondo_t *)malloc(sizeof(fondo_t));
+  fondo->bg_image = al_load_bitmap(BACK);
+  fondo->x = 0;
+  fondo->y = 0;
+  al_draw_bitmap(fondo->bg_image, 0, 0, 0);
+  return fondo;
+}
