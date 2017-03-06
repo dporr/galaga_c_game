05ba915fd5f1ee6b9b5f69829a6b8b34d23e7ec4
Diego Porras <andree.rivas@galileo.edu>
2017-03-05 22:27:10 -0600

    Logica disparos player

--- player.h
+++ player.h
@@ -1,17 +1,27 @@
 #define PLAYER_SHIP "res/spacecraft.png"
 #define PLAYER_EXPLODE "res/expl.png"
+#define P_MAX_BULLET 10
 typedef struct jugador {
   int x; // posicion x de la nave
   int y; // posicion y de la nave
   ALLEGRO_BITMAP *nave; //imagen a renderizar
 } jugador_t;
+
+typedef struct pbullet {
+  int x; // posicion x de la nave
+  int y; // posicion y de la nave
+  ALLEGRO_BITMAP *bullet_bmp; //imagen a renderizar
+} pbullet_t;
+pbullet_t** p_bullets;
+int pn_bullets = 0;
 //Prototipo funciones jugador
 void dibujarJugador(jugador_t *jugador);
 void moverArriba(jugador_t *jugador);
 void moverAbajo(jugador_t *jugador);
 void moverDerecha(jugador_t *jugador);
 void moverIzquierda(jugador_t *jugador);
-
+void draw_p_shoot();
+void p_shoot();
 jugador_t* player;
 jugador_t* init_player(){
   jugador_t* player = (jugador_t *)malloc(sizeof(jugador_t));
@@ -71,4 +81,37 @@ void moverIzquierda(jugador_t *jugador) {
     jugador->x -= 50 ;    
   }
   dibujarJugador(jugador);
+}
+
+void p_shoot(){  
+  if(pn_bullets<P_MAX_BULLET){
+    p_bullets[pn_bullets] = (pbullet_t*) malloc(sizeof(pbullet_t));
+    (p_bullets[pn_bullets])->x = player->x + 10;
+    (p_bullets[pn_bullets])->y = player->y + 10;
+    (p_bullets[pn_bullets])->bullet_bmp = al_load_bitmap(BULLET_IMG);
+    pn_bullets++;
+  }
+  draw_p_shoot();
+}
+
+void draw_p_shoot(){
+  if(pn_bullets<P_MAX_BULLET){
+    for(int j = 0;j<pn_bullets;j++){
+      if(!((p_bullets[j])->y >= 600)){
+        (p_bullets[j])->y+=10;
+        al_draw_scaled_bitmap((p_bullets[j])->bullet_bmp,
+        0,0,128,128,
+        (p_bullets[j])->x,(p_bullets[j])->y,
+        20,20,ALLEGRO_FLIP_VERTICAL); 
+        //free(p_bullets[j])
+        //p_bullets[j] = (bullet_t*) malloc(sizeof(bullet_t));;
+        continue;
+      }
+      if(j < pn_bullets-1){
+        p_bullets[j] = p_bullets[j+1];
+        j--;
+        pn_bullets--;
+      }
+    }
+  }
 }
